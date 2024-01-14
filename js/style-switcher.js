ecece7718f1757225545dcd6daf683f66096f9df
Mattycp <155780794+Mattycp@users.noreply.github.com>
2024-01-14 19:18:46 -0300

    Add files via upload

---
+++ js/style-switcher.js
@@ -0,0 +1,41 @@
+/* ====================================== ativar a troca de teme ==================================*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+// hide style switche on scroll
+window.addEventListener("scroll", () =>{
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+/* ====================================== cores do tema ==================================*/
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }else{
+            style.setAttribute("disabled","true");
+        }
+    })
+}
+
+/* ====================================== tema claro e escuro ==================================*/
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () =>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark")
+})
+
+window.addEventListener("load", () =>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
